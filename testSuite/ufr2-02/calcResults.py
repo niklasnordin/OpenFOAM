8cba869ea2775a6b9df0e6b825b606bb053bf511
Niklas Nordin <niklas.nordin@nequam.se>
2018-06-07 10:24:40 +0200

    calculates St and Cd

--- testSuite/ufr2-02/calcResults.py
+++ testSuite/ufr2-02/calcResults.py
@@ -1,10 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy.fft as fft
+import numpy as np
+
+D = 0.04
+U = 0.54
+def calcZeroCrossing(x, y):
+    t = []
+    n = len(x)
+    for i in range(0, n-1):
+        y0 = y[i]
+        y1 = y[i+1]
+        if y0 < 0 and y1 > 0:
+            t0 = x[i]
+            t1 = x[i+1]
+            tc = t0 - y0*(t1-t0)/(y1-y0)
+            t.append(tc)
+    return t
 
 data = pd.read_csv("postProcessing/forces/0.000000e+00/coefficient_0.000000e+00.dat", skiprows=10, delimiter='\t', header=None)
-
-
 
 t = data[0]
 cm = data[1]
@@ -12,16 +25,24 @@ cd = data[2]
 cl = data[3]
 clf = data[4]
 clr = data[5]
-signal = [t, cl]
-spectrum = fft.fft(signal)
-n = len(spectrum)
-freq = fft.fftfreq(n)
-print(freq)
-print(spectrum)
+s = cl
 
+times = calcZeroCrossing(t, s)
+n = len(times)
+dt = []
+for i in range(0,n-1):
+    dti = times[i+1]-times[i]
+    dt.append(dti)
 
+dt_calc = dt[30:]
+dt_sum = sum(dt_calc)
+
+cd_calc = cd[10000:]
+cd_av = sum(cd_calc)/len(cd_calc)
+av_dt = dt_sum/len(dt_calc)
+St = D/(av_dt*U)
+print("St = {}, Cd = {}".format(St, cd_av))
 #fig, ax = plt.subplots()
-#scat = ax.scatter(t,cl,s=1)
+#plt.plot(cd[10000:],'o')
 #ax.grid()
-#ax.set_xlabel("Time [s]")
 #plt.show()
